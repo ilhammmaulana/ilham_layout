4172e9058fb623ec2aa4d5fb93309336e4f8e532
ilhammmaulana <ilhammmaulana688@gmail.com>
2023-02-25 17:05:20 +0700

    frontend

--- resources/views/components/web/footer.blade.php
+++ resources/views/components/web/footer.blade.php
@@ -14,10 +14,10 @@
             <p class="mx-1 text-muted">Ketok Magic laravel</p>
         </div>
         <div class="col-md-4">
-            <h4 class="fw-bold">Layanan</h4>
-            <p class="mx-1 text-muted">Periksa kodingan</p>
-            <p class="mx-1 text-muted">Service pattern</p>
-            <p class="mx-1 text-muted">Ketok Magic laravel</p>
+            <h4 class="fw-bold">Suport</h4>
+            <p class="mx-1 text-muted">Help</p>
+            <p class="mx-1 text-muted">Tearms and service</p>
+            <p class="mx-1 text-muted">Private Policy</p>
         </div>
         <h6 class="text-muted fw-600 text-center p-5">Created By Ilham Maulana - Kelas Industri</h6>
     </div>
