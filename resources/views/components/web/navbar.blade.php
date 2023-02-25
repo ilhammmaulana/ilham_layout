99c43bb66d8d11924766a0a4daa90cccb1e842b5
ilhammmaulana <ilhammmaulana688@gmail.com>
2023-02-25 18:12:04 +0700

    github addiing ling

--- resources/views/components/web/navbar.blade.php
+++ resources/views/components/web/navbar.blade.php
@@ -4,10 +4,10 @@
             <img src={{ asset('assets/images/image1.png') }} alt="">
         </a>
       <div class="d-flex gap-3" role="search">
-        <a href="">
+        <a href="https://github.com/ilhammmaulana">
           <button class="btn btn-outline-warning">Download App</button>
         </a>
-        <a href="nav-link">
+        <a href="https://github.com/ilhammmaulana">
             <button class="btn btn-warning text-white">Login</button>
         </a>
       </div>
