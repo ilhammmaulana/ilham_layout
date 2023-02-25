9eb9dda6840a50132e8d86330e51644f90d25d3a
ilhammmaulana <ilhammmaulana688@gmail.com>
2023-02-25 14:13:40 +0700

    Adding text

--- resources/views/web/pages/home.blade.php
+++ resources/views/web/pages/home.blade.php
@@ -1,6 +1,7 @@
 <x-web.layouts>
     <x-slot name="title">Home</x-slot>
     <x-slot name="html">
+        <h4>Ilham Maulana</h4>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste quae necessitatibus quia iusto non repellendus provident, molestiae tempore earum deserunt!</p>
     </x-slot>
 </x-web.layouts>
